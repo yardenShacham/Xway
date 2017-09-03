797cf05cea64b7a4d8663624cdf016b1fdb8e58d
yarde_000 <yarden101111@gmail.com>
2017-09-04 00:54:04 +0300

    fixed stuff

--- tools/build.js
+++ tools/build.js
@@ -3,38 +3,41 @@
 const fs = require('fs');
 const del = require('del');
 const rollup = require('rollup');
-const typescript = require('rollup-plugin-typescript');
 const pkg = require('../package.json');
+const babel = require('rollup-plugin-babel');
+const uglify = require('rollup-plugin-uglify');
+const minify = require('uglify-es').minify;
 
-let promise = Promise.resolve();
 
 // Clean up the output directory
-promise = promise.then(() => del(['dist/*']));
-
 // Compile source code into a distributable format with Babel
-['es', 'cjs', 'umd'].forEach((format) => {
-    promise = promise.then(() => rollup.rollup({
-        entry: 'src/index.ts',
-        external: Object.keys(pkg.dependencies),
-        plugins: [ babel({
-            exclude: 'node_modules/**'
-        })],
-    }).then(bundle => bundle.write({
-        dest: `dist/${format === 'cjs' ? 'xway' : `xway.${format}`}.js`,
-        format,
-        sourceMap: true,
-        moduleName: format === 'umd' ? pkg.name : undefined,
-    })));
-});
-
 // Copy package.json and LICENSE.txt
-promise = promise.then(() => {
-    delete pkg.private;
-    delete pkg.devDependencies;
-    delete pkg.scripts;
-    delete pkg.babel;
-    fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, '  '), 'utf-8');
-    fs.writeFileSync('dist/LICENSE.txt', fs.readFileSync('LICENSE.txt', 'utf-8'), 'utf-8');
-});
-
-promise.catch(err => console.error(err.stack));
+Promise.resolve().then(() => del(['dist/*']))
+    .then(() => rollup.rollup({
+        entry: 'src/index.js',
+        external: Object.keys(pkg.dependencies),
+        plugins: [],
+    }).then(bundle => bundle.write({
+        dest: `dist/xway.umd.js`,
+        format: "umd",
+        sourceMap: true,
+        moduleName: pkg.name,
+    })).then(() => rollup.rollup({
+        entry: 'src/index.js',
+        external: Object.keys(pkg.dependencies),
+        plugins: [
+            uglify({}, minify)
+        ],
+    }).then(bundle => bundle.write({
+        dest: `dist/xway.umd.min.js`,
+        format: "umd",
+        sourceMap: true,
+        moduleName: pkg.name,
+    })).then(() => {
+        delete pkg.private;
+        delete pkg.devDependencies;
+        delete pkg.scripts;
+        delete pkg.babel;
+        fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, '  '), 'utf-8');
+        fs.writeFileSync('dist/LICENSE', fs.readFileSync('LICENSE', 'utf-8'), 'utf-8');
+    }).catch(err => console.error(err.stack))));
